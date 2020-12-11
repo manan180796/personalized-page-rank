4b881f7084496e8f6c515d619b3a575305972ab4
manan180796 <manan180796@gmail.com>
2020-12-12 00:57:00 +0530

    visualization

--- page-rank.py
+++ page-rank.py
@@ -3,27 +3,34 @@ import utils
 from matplotlib import pyplot as plt
 import os
 import numpy as np
-output_dir = ".output"
 
-# graph: nx.Graph = nx.petersen_graph()
-# graph: nx.Graph = nx.karate_club_graph()
-graph: nx.Graph = nx.grid_graph((10,10))
-for node in graph.nodes:
-    graph.add_edge(node,node)
-features = np.random.rand(10,10)
-features[:,:] = 0.0001
-features[0,0] = 1.0
-# features = np.random.rand(nx.number_of_nodes(graph))
+output_dir = ".output"
+graph: nx.Graph = nx.grid_graph((10, 10))
+# graph=nx.Graph(mode="slice",timerepresentation="timestamp" ,timestamp="1")
+features = np.zeros((10, 10))
+features[0, 0] = 1
 features = features/np.sum(features)
-my_layout = nx.kamada_kawai_layout(graph)
+
+history = nx.Graph()
+
+for node in graph:
+    graph.add_edge(node,node)
+
+# my_layout = nx.kamada_kawai_layout(G=graph)
+
 for i in range(1000):
-    features = utils.diffuse(graph, features)
-    if(i%10==0):
-        fig = plt.figure(1, figsize=(6, 6))
-        # nx.draw(G=graph,pos=my_layout,node_color=colors,vmin=0.0,vmax=1.0)
-        # print(features)
-        nx.draw(G=graph, pos=my_layout, node_color=np.log(features))
-        # plt.show()
-        plt.savefig(os.path.join(output_dir, str(i)+".png"))
-        plt.close()
-    # break
+    features = utils.diffuse(graph=graph, features=features)
+    # print(i)
+    if i % 10 == 0:
+        for node in graph:
+            x, y = node
+            history.add_node(
+                (node, i), pr=np.log(features[node]), start=i, end=i+10, viz={"position": {"x": 100*x, "y": 100*y}})
+        for edge in graph.edges.data("weight"):
+            # print(edge)
+            # break
+            u,v,_=edge
+            history.add_edge((u, i), (v, i), weight=None)
+        # nx.write_gexf(graph, os.path.join(output_dir, str(i)+".gexf"),version="1.2draft")
+nx.write_gexf(history, os.path.join(
+    output_dir, "graph.gexf"), version="1.2draft")
--- utils.py
+++ utils.py
@@ -3,7 +3,8 @@ import numpy as np
 
 def diffuse(graph: nx.Graph,features:np.array) -> np.array:
     new_features = np.zeros(np.shape(features))
-    for node in graph.nodes():
-        for neigh in graph.neighbors(node):
-            new_features[node] += (features[neigh]/graph.degree[neigh])
+    for node,neigh_dict in graph.adjacency():
+        for neigh,attr in neigh_dict.items():
+            new_features[node]+= features[neigh]/graph.degree[node]
+    # print(new_features)
     return new_features
