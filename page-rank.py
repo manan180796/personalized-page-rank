86d77e09f958414bf3b43656d88017195629bc26
manan180796 <manan180796@gmail.com>
2020-12-15 00:21:50 +0530

    added teleport diffusion

--- page-rank.py
+++ page-rank.py
@@ -1,36 +1,74 @@
-import networkx as nx
-import utils
-from matplotlib import pyplot as plt
+# from typing import Type
+import csv
+import io
 import os
+import urllib.request as urllib
+import zipfile
+
+import networkx as nx
 import numpy as np
 
-output_dir = ".output"
-graph: nx.Graph = nx.grid_graph((10, 10))
-# graph=nx.Graph(mode="slice",timerepresentation="timestamp" ,timestamp="1")
-features = np.zeros((10, 10))
-features[0, 0] = 1
+import utils
+epsilon = 1e-9
+
+
+# url = "http://www-personal.umich.edu/~mejn/netdata/football.zip"
+
+# sock = urllib.urlopen(url)  # open URL
+# s = io.BytesIO(sock.read())  # read into BytesIO "file"
+# sock.close()
+
+# zf = zipfile.ZipFile(s)  # zipfile object
+# txt = zf.read("football.txt").decode()  # read info file
+# gml = zf.read("football.gml").decode()  # read gml data
+# # throw away bogus first line with # from mejn files
+# gml = gml.split("\n")[1:]
+# graph = nx.parse_gml(gml)  # parse gml data
+
+
+graph: nx.Graph = nx.karate_club_graph()
+
+utils.enumerate_nodes(graph=graph)
+
+
+features = np.zeros(graph.number_of_nodes())
+features[0] = 1
 features = features/np.sum(features)
 
-history = nx.Graph()
+alphas = [0.6, 0.7, 0.85, 0.9, 0.95, 1.0]
+teleport = np.copy(features)
+
 
 for node in graph:
-    graph.add_edge(node,node)
+    # graph.add_edge(node, node)
+    pass
 
-# my_layout = nx.kamada_kawai_layout(G=graph)
 
-for i in range(1000):
-    features = utils.diffuse(graph=graph, features=features)
-    # print(i)
-    if i % 10 == 0:
-        for node in graph:
-            x, y = node
-            history.add_node(
-                (node, i), pr=np.log(features[node]), start=i, end=i+10, viz={"position": {"x": 100*x, "y": 100*y}})
-        for edge in graph.edges.data("weight"):
-            # print(edge)
-            # break
-            u,v,_=edge
-            history.add_edge((u, i), (v, i), weight=None)
-        # nx.write_gexf(graph, os.path.join(output_dir, str(i)+".gexf"),version="1.2draft")
-nx.write_gexf(history, os.path.join(
-    output_dir, "graph.gexf"), version="1.2draft")
+pr_features = {a: np.copy(features) for a in alphas}
+# ppr_features = np.copy(features)
+
+diffuzer = {
+    key: utils.TeleportDiffusion(
+        diffuzer=utils.NormalizedDiffusion(),
+        teleport_vector=teleport,
+        tele_prob=key
+    )
+    for key in alphas
+}
+
+
+for i in range(100):
+    for key, cur_features in pr_features.items():
+        pr_features[key] = diffuzer[key](graph=graph, features=cur_features)
+
+result = {"original": features}
+
+for key, cur_features in pr_features.items():
+    result[f"pr(alpha={key})"] = cur_features
+    result[f"pr(alpha={key}).log"] = np.log(cur_features+epsilon)
+
+utils.set_numpy_attribute(
+    graph=graph,
+    attr=result
+)
+nx.write_gexf(graph, ".output/karate_club_graph.gexf")
--- utils.py
+++ utils.py
@@ -1,10 +1,52 @@
 import networkx as nx
 import numpy as np
+from typing import List, Dict
+import os
+import csv
 
-def diffuse(graph: nx.Graph,features:np.array) -> np.array:
-    new_features = np.zeros(np.shape(features))
-    for node,neigh_dict in graph.adjacency():
-        for neigh,attr in neigh_dict.items():
-            new_features[node]+= features[neigh]/graph.degree[node]
-    # print(new_features)
-    return new_features
+
+class Diffusion:
+    def __call__(self, graph: nx.Graph, features: np.array) -> np.array:
+        new_features = np.zeros(np.shape(features))
+        for node, neigh_dict in graph.adjacency():
+            u = graph.nodes[node]["index"]
+            for neigh in neigh_dict:
+                v = graph.nodes[neigh]["index"]
+                new_features[v] += features[u]/graph.degree[node]
+        return new_features
+
+
+class NormalizedDiffusion(Diffusion):
+    def __call__(self, graph: nx.Graph, features: np.array) -> np.array:
+        new_features = np.zeros(np.shape(features))
+        for node, neigh_dict in graph.adjacency():
+            u = graph.nodes[node]["index"]
+            for neigh in neigh_dict:
+                v = graph.nodes[neigh]["index"]
+                new_features[v] += features[u] / \
+                    np.sqrt(graph.degree[node]*graph.degree[neigh])
+        return new_features
+
+
+class TeleportDiffusion(Diffusion):
+    def __init__(self, diffuzer: Diffusion, teleport_vector: np.array, tele_prob: float = 0.0) -> None:
+        self.diffuzer = diffuzer
+        self.teleport_vector = teleport_vector
+        self.tele_prob = tele_prob
+
+    def __call__(self, graph: nx.Graph, features: np.array) -> np.array:
+        new_features = self.diffuzer(graph=graph, features=features)
+        new_features = (1-self.tele_prob)*new_features + \
+            self.tele_prob*self.teleport_vector
+        return new_features
+
+
+def enumerate_nodes(graph: nx.Graph):
+    for i, node in enumerate(graph):
+        graph.nodes[node]["index"] = i
+
+
+def set_numpy_attribute(graph: nx.Graph, attr: Dict[str, np.array]):
+    for node in graph:
+        for key, value in attr.items():
+            graph.nodes[node][key] = value[graph.nodes[node]["index"]]
